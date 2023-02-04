9a33fa287408f59cbe51dda3e8ff7b6febd4db90
Olga <helga.yudina@gmail.com>
2023-02-04 18:47:05 +0500

    task3

--- Seminar2_task3.py
+++ Seminar2_task3.py
@@ -2,14 +2,16 @@
 # Реализовать проверку типа данных каждого элемента.
 # Использовать функцию type() для проверки типа.
 # Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.
-#
 # Пример:
 # для списка [5, "string", 0.15, True, None]
 # результат
-#
 # <class 'int'>
 # <class 'str'>
 # <class 'float'>
 # <class 'bool'>
 # <class 'NoneType'>
 
+my_list = [5, "string", 0.15, True, None]
+print(my_list)
+for i in my_list:
+    print(f'{i} is {type(i)}')
