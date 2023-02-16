7f5c55783ebd4d2256e258ebbd1a574d0b4339f3
Olga <helga.yudina@gmail.com>
2023-02-16 22:02:52 +0500

    task4

--- Seminar7_task4.py
+++ Seminar7_task4.py
@@ -12,3 +12,75 @@
 # Создайте экземпляры классов, передайте значения атрибутов.
 # Выполните доступ к атрибутам, выведите результат.
 # Выполните вызов методов и также покажите результат.
+
+class Car:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        return f'{self.name} поехала'
+
+    def stop(self):
+        return f'{self.name} остановилась'
+
+    def turn_right(self):
+        return f'{self.name} повернула направо'
+
+    def turn_left(self):
+        return f'{self.name} повернула налево'
+
+    def show_speed(self):
+        return f'Текущая скорость автомобиля {self.name} - {self.speed}'
+
+
+class TownCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Текущая скорость городского автомобиля {self.name} - '
+              f' {self.speed}')
+
+        if self.speed > 60:
+            return f'Скорость {self.name} превышена'
+        else:
+            return f'Скорость {self.name} в норме'
+
+
+class WorkCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Текущая скорость рабочего автомобиля {self.name} - '
+              f' {self.speed}')
+        if self.speed > 40:
+            return f'Скорость {self.name} превышена'
+        else:
+            return f'Скорость {self.name} в норме'
+
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+kia = TownCar(60, 'Black', 'KIA', False)
+ferrari = SportCar(180, 'Red', 'Ferrari', False)
+skoda = WorkCar(50, 'Grey', 'Skoda', False)
+ford = PoliceCar(90, 'White',  'Ford', True)
+
+print(f'{skoda.show_speed()}')
+print(f'{kia.show_speed()}')
+print(f'Когда {ferrari.go()}, {skoda.stop()}')
+print(f'{ford.name} полицейская машина? {ford.is_police}')
+print(f'{ferrari.name} полицейская машина?  {ferrari.is_police}')
