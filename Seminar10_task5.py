63d0db5bcb0f7ef7980cf4fb94a73b2d6321d4e9
Olga <helga.yudina@gmail.com>
2023-02-25 22:06:43 +0500

    task5

--- Seminar10_task5.py
+++ Seminar10_task5.py
@@ -7,3 +7,20 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+
+import subprocess
+import chardet
+
+args = ['ping', 'yandex.ru']
+yandex_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+for line in yandex_ping.stdout:
+    result = chardet.detect(line)
+    line = line.decode(result['encoding']).encode('utf-8')
+    print(line.decode('utf-8'))
+
+args = ['ping', 'youtube.com']
+youtube_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+for line in youtube_ping.stdout:
+    result = chardet.detect(line)
+    line = line.decode(result['encoding']).encode('utf-8')
+    print(line.decode('utf-8'))
